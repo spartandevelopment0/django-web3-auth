7090db6ce338239408c908753b9468b54729fcd7
welhoilija <tkankas@gmail.com>
2022-04-26 18:53:09 +0300

    Update views.py

--- web3auth/views.py
+++ web3auth/views.py
@@ -7,7 +7,6 @@ from django.contrib.auth import login, authenticate
 from django.http import JsonResponse
 from django.shortcuts import render, redirect, reverse
 from django.urls.exceptions import NoReverseMatch
-from django.utils.translation import ugettext_lazy as _
 from django.views.decorators.http import require_http_methods
 
 from web3auth.forms import LoginForm, SignupForm
@@ -36,8 +35,8 @@ def login_api(request):
     else:
         token = request.session.get('login_token')
         if not token:
-            return JsonResponse({'error': _(
-                "No login token in session, please request token again by sending GET request to this url"),
+            return JsonResponse({'error': 
+                "No login token in session, please request token again by sending GET request to this url",
                 'success': False})
         else:
             form = LoginForm(token, request.POST)
@@ -50,8 +49,7 @@ def login_api(request):
 
                     return JsonResponse({'success': True, 'redirect_url': get_redirect_url(request)})
                 else:
-                    error = _("Can't find a user for the provided signature with address {address}").format(
-                        address=address)
+                    error = "Can't find a user for the provided signature with address %s" % address
                     return JsonResponse({'success': False, 'error': error})
             else:
                 return JsonResponse({'success': False, 'error': json.loads(form.errors.as_json())})
@@ -60,7 +58,7 @@ def login_api(request):
 @require_http_methods(["POST"])
 def signup_api(request):
     if not app_settings.WEB3AUTH_SIGNUP_ENABLED:
-        return JsonResponse({'success': False, 'error': _("Sorry, signup's are currently disabled")})
+        return JsonResponse({'success': False, 'error': "Sorry, signup's are currently disabled"})
     form = SignupForm(request.POST)
     if form.is_valid():
         user = form.save(commit=False)
@@ -89,7 +87,7 @@ def signup_view(request, template_name='web3auth/signup.html'):
     """
     form = SignupForm()
     if not app_settings.WEB3AUTH_SIGNUP_ENABLED:
-        form.add_error(None, _("Sorry, signup's are currently disabled"))
+        form.add_error(None, "Sorry, signup's are currently disabled")
     else:
         if request.method == 'POST':
             form = SignupForm(request.POST)
