26089cbc83ff27e93633263ddb040b713fdaec24
Welhoilija <=>
2022-04-26 19:37:59 +0300

    updated urls

--- web3auth/urls.py
+++ web3auth/urls.py
@@ -5,7 +5,7 @@ from web3auth import views
 app_name = 'web3auth'
 
 urlpatterns = [
-    path(r'^login_api/$', views.login_api, name='web3auth_login_api'),
-    path(r'^signup_api/$', views.signup_api, name='web3auth_signup_api'),
-    path(r'^signup/$', views.signup_view, name='web3auth_signup'),
+    path('login_api/', views.login_api, name='web3auth_login_api'),
+    path('signup_api/', views.signup_api, name='web3auth_signup_api'),
+    path('signup/', views.signup_view, name='web3auth_signup'),
 ]
